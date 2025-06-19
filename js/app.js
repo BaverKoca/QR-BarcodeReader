e2247712cc9c37a128201d11a32e1c4657b2ff85
Baver Koca <baver.koca00@gmail.com>
2025-06-19 19:13:55 +0100

    now oepn camera and scan then show url it support QR_CODE, CODE_128, CODE_39, CODE_93, EAN_13, EAN_8, UPC_A, UPC_E, ITF, AZTEC, DATA_MATRIX and PDF_41 formats

--- js/app.js
+++ js/app.js
@@ -32,7 +32,24 @@ async function startCamera() {
   try {
     await html5QrCode.start(
       { facingMode: "environment" },
-      { fps: 15, qrbox: { width: 250, height: 250 } },
+      {
+        fps: 15,
+        qrbox: { width: 300, height: 200 },
+        formatsToSupport: [
+          Html5QrcodeSupportedFormats.QR_CODE,
+          Html5QrcodeSupportedFormats.CODE_128,
+          Html5QrcodeSupportedFormats.CODE_39,
+          Html5QrcodeSupportedFormats.CODE_93,
+          Html5QrcodeSupportedFormats.EAN_13,
+          Html5QrcodeSupportedFormats.EAN_8,
+          Html5QrcodeSupportedFormats.UPC_A,
+          Html5QrcodeSupportedFormats.UPC_E,
+          Html5QrcodeSupportedFormats.ITF,
+          Html5QrcodeSupportedFormats.AZTEC,
+          Html5QrcodeSupportedFormats.DATA_MATRIX,
+          Html5QrcodeSupportedFormats.PDF_417
+        ]
+      },
       onScanSuccess,
       onScanFailure
     );
